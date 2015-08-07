6efdbb0cd98514f56e96e3d5daa8eb835567d632
Mara Ramsden <m.ramsden90@gmail.com>
2015-08-07 10:08:14 -0400

    fixed root again

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'welcome/index'
-
-  root 'people#index'
+BoothLeads::Application.routes.draw do
+   resources :leads, path: ''
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
